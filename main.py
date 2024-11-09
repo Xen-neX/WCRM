61afc8fd735847567940a16faf808e798e14b311
Xen-neX <elaginnn@gmail.com>
2024-11-09 20:38:04 +0300

    refactoring

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def response(url: str, headers: dict) -> requests.Response:
     Returns:
         requests.Response: Объект ответа
     """
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers, timeout=5)
     resp.raise_for_status()  # Проверяем статус-код ответа
     return resp
 
@@ -32,19 +32,19 @@ def main(url: str, headers: dict) -> dict | str:
             try:
                 return data.json()
             except json.JSONDecodeError as e:
-                return f"Ошибка парсинга JSON: {e}"  # Сразу возвращаем ошибку без повторных попыток
-                
+                return f"Ошибка парсинга JSON: {e}"
         except requests.RequestException as e:
             if attempt < ATTEMPTS - 1:
                 print(f"Попытка {attempt + 1} не удалась. Повторная попытка...")
                 time.sleep(1)
             else:
                 return f"Ошибка получения данных: {e}"
+    return "Превышено максимальное количество попыток"
 
 
 if __name__ == "__main__":
     result = main(
-        "https://dev.whatcrm.net/v3/tariffs?currency=RUB&crm=lk",
+        "https://dev.whatc3rm.net/v3/tariffs?currency=RUB&crm=lk",
         {"X-Whatsapp-Token": "5d8af8faaeb61680883a850be0c577e2"},
     )
     print(json.dumps(result, indent=4, ensure_ascii=False))
